12c648722ee1b5969ef501eaa57a2a08113fca26
rjwelshinger <rjwelshinger@gmail.com>
2015-06-21 19:10:41 -0500

    This is a message 6-21-2015

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,58 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# The MakeCacheMatrix function creates and provides set and get functions for a matrix i
+
+makeCacheMatrix <- function(matrixCache = matrix()) {
+     
+   
+     set <- function(y) {
+          # set the set the matrixCache in the global environment and null the inverse chache, 
+          # also in the global environment
+          staticMatrixCache <<- y)
+          inverseMatrixCache <<- NULL
+     }
+     # pass back matrix
+     get <- function() staticMatrixCache
+     
+     # use R, solve function to calculate the invers of the matirx in matrix 
+     setInverse <-  function(solve) inverseMatrixCache <<- solve
+     
+     getInverse <- function() inverseMatrixCache
+
+     list(set = set, get = get,
+          setInverse  = setInverse ,
+          getInverse  = getInverse )
+     
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, f) {
         ## Return a matrix that is the inverse of 'x'
+     # initialize return variable as an empty matrix
+     ##browser()
+     ret <- NULL
+     ccc <- f()$get()
+     bbb <- x
+     if(!identical(bbb, ccc)){
+          
+          f()$set(bbb)
+
+          inverseMatrixCache <- solve(x)
+         
+          
+     }else{
+          if (is.null(inverseMatrixCache)) {
+               f()$setInverse( solve(x))
+          }
+          ret <- f()$getInverse()
+     }
+     
+     ret
 }
+
+
+
